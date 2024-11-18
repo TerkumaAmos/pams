11ca87c2643e7399259fa7aa654c205bb335a663
TerkumaAmos <agberterkuma2020@gmail.com>
2024-11-18 22:41:26 +0100

    new

--- lib/main.dart
+++ lib/main.dart
@@ -13,71 +13,39 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
             // title: Text('Multiple Text Widgets'),
             ),
-        body: Center(
-          child: Container(
-            margin: const EdgeInsets.all(16),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  padding: const EdgeInsets.all(
-                    16.0,
-                  ),
-                  decoration: const BoxDecoration(color: Colors.black),
-                  child: const Text(
-                    'PAMS',
-                    style: TextStyle(fontSize: 20, color: Colors.yellow),
-                  ),
-                ),
-                const Text(
-                  '''Pipeline Activation
-Management System 
-(PAMS)
-            ''',
-                  style: TextStyle(
-                      color: Colors.black, fontWeight: FontWeight.bold),
-                ),
-                Text('Email Address'),
-                const TextField(
-                    decoration: InputDecoration(
-                  hintText: 'Enter Your Email',
-                  hintStyle: TextStyle(color: Colors.black),
-                  filled: true,
-                  // focusColor: Colors.black,
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      width: 2.0,
-                      style: BorderStyle.solid,
-                      strokeAlign: BorderSide.strokeAlignOutside,
+        body: SingleChildScrollView(
+          child: Center(
+            child: Container(
+              margin: const EdgeInsets.all(16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(
+                      16.0,
+                    ),
+                    decoration: const BoxDecoration(color: Colors.black),
+                    child: const Text(
+                      'PAMS',
+                      style: TextStyle(fontSize: 20, color: Colors.yellow),
                     ),
                   ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      width: 2.0,
-                      style: BorderStyle.solid,
-                      strokeAlign: BorderSide.strokeAlignOutside,
-                    ),
+                  const Text(
+                    '''Pipeline Activation
+          Management System 
+          (PAMS)
+              ''',
+                    style: TextStyle(
+                        color: Colors.black, fontWeight: FontWeight.bold),
                   ),
-                )
-                    //  focusedBorder:border
-                    ),
-                const SizedBox(
-                  height: 20,
-                  width: 20,
-                ),
-                const Text(
-                  'Password',
-                  style: TextStyle(
-                      color: Colors.black, fontWeight: FontWeight.bold),
-                ),
-                const TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Enter Your Password',
-                    hintStyle: TextStyle(
-                      color: Colors.black,
-                    ),
+                  Text('Email Address'),
+                  const TextField(
+                      decoration: InputDecoration(
+                    hintText: 'Enter Your Email',
+                    hintStyle: TextStyle(color: Colors.black),
                     filled: true,
+                    // focusColor: Colors.black,
                     focusedBorder: OutlineInputBorder(
                       borderSide: BorderSide(
                         width: 2.0,
@@ -87,38 +55,77 @@ Management System
                     ),
                     enabledBorder: OutlineInputBorder(
                       borderSide: BorderSide(
-                          width: 2.0,
-                          style: BorderStyle.solid,
-                          strokeAlign: BorderSide.strokeAlignOutside),
+                        width: 2.0,
+                        style: BorderStyle.solid,
+                        strokeAlign: BorderSide.strokeAlignOutside,
+                      ),
+                    ),
+                  )
+                      //  focusedBorder:border
+                      ),
+                  const SizedBox(
+                    height: 20,
+                    width: 20,
+                  ),
+                  const Text(
+                    'Password',
+                    style: TextStyle(
+                      color: Colors.black,
                     ),
                   ),
-                ),
-                const SizedBox(
-                  width: 30,
-                  height: 40,
-                ),
-                Container(
-                  child: TextButton(
-                    onPressed: () {},
-                    child: Text('LOGIN TO MY ACCOUNT'),
+                  const TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Enter Your Password',
+                      hintStyle: TextStyle(
+                        color: Colors.black,
+                      ),
+                      filled: true,
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          width: 2.0,
+                          style: BorderStyle.solid,
+                          strokeAlign: BorderSide.strokeAlignOutside,
+                        ),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            width: 2.0,
+                            style: BorderStyle.solid,
+                            strokeAlign: BorderSide.strokeAlignInside),
+                      ),
+                    ),
                   ),
-                  decoration: BoxDecoration(
-                    color: Colors.yellow,
+                  const SizedBox(
+                    width: 30,
+                    height: 40,
                   ),
-                ),
-                SizedBox(
-                  width: 20,
-                  height: 30,
-                ),
-                const Center(
-                  child: Text(
-                    'Forgot password',
-                    style: TextStyle(
-                        decoration: TextDecoration.underline,
-                        color: Colors.orange),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 100),
+                    decoration: const BoxDecoration(
+                      color: Colors.yellow,
+                    ),
+                    child: TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        'LOGIN TO MY ACCOUNT',
+                      ),
+                    ),
                   ),
-                ),
-              ],
+                  const SizedBox(
+                    width: 20,
+                    height: 30,
+                  ),
+                  const Center(
+                    child: Text(
+                      'Forgot password',
+                      style: TextStyle(
+                          decoration: TextDecoration.underline,
+                          decorationColor: Colors.orange,
+                          color: Colors.orange),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
